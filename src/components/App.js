f4ed369f623cb5f6fe9f8e5bdf081c5a46db1370
AshishDeshmukh018 <126492153+AshishDeshmukh018@users.noreply.github.com>
2023-03-16 12:36:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,26 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+        function DiwaliShoppingList() {
+  const relativesList = [
+    { key: "relativeListItem1", name: "Amit" },
+    { key: "relativeListItem2", name: "Sonal" },
+    { key: "relativeListItem3", name: "Neha" },
+    { key: "relativeListItem4", name: "Rajesh" },
+    { key: "relativeListItem5", name: "Aparna" },
+  ];
+
+  return (
+    <div>
+      <h2>Relatives to Visit During Diwali</h2>
+      <ol>
+        {relativesList.map((relative) => (
+          <li key={relative.key}>{relative.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
     </div>
   )
 }
